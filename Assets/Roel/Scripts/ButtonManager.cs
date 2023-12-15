ed377c2f1882c96035350bb69d366256f9e7d256
Roel de Bruijn <roel.de.bruijn@gmail.com>
2023-12-15 10:21:21 +0100

    main menu
    meerder panels en begin maken aan het switchen tussen panels

---
+++ Assets/Roel/Scripts/ButtonManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject settingMenu;
+    public GameObject videoSettingMenu;
+    public GameObject creditsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToSettings()
+    {
+        mainMenu.SetActive(false);
+        settingMenu.SetActive(true);
+    }
+
+    public void GoToCredits()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+    public void GoToMainMenu() 
+    {
+        settingMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void GoToBackSettings()
+    {
+        videoSettingMenu.SetActive(false);
+        settingMenu.SetActive(true);
+    }
+}
