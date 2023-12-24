dbf40f7d86fd3d58bc8a1f9f15184f377d5c20da
Florisvdk <floris_2@hotmail.nl>
2023-12-24 15:45:01 +0100

    UI
    ui gekoppeld + raycast

---
+++ Assets/RaycastHand.cs
@@ -0,0 +1,73 @@
+using EasyRoads3Dv3;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class RaycastHand : MonoBehaviour
+{
+    public float rayLenght = 100f;
+    RaycastHit hit;
+    private LineRenderer lineRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        if(lineRenderer == null)
+        {
+            lineRenderer = gameObject.AddComponent<LineRenderer>();
+        }
+        lineRenderer.material = new Material(Shader.Find("Standard"));
+        lineRenderer.startColor = Color.green;
+        lineRenderer.endColor = Color.green;
+        lineRenderer.startWidth = 0.009f;
+        lineRenderer.endWidth = 0.009f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        PeformRaycast();
+        //CheckUIInteraction();
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1,transform.position + transform.forward * rayLenght);
+        
+    }
+    void PeformRaycast()
+    {
+        int layerMask = 1 << LayerMask.NameToLayer("UI");
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        if(Physics.Raycast(ray, out hit, rayLenght,layerMask))
+        {
+            if (hit.collider.tag == "Settings")
+            {
+                if (OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.LTouch))
+                {
+
+                    Button button = hit.collider.GetComponent<Button>();
+                    Dropdown dropdown = hit.collider.GetComponent<Dropdown>();
+                    if (button != null)
+                    {
+                        button.onClick.Invoke();
+                    }
+                    else if(dropdown != null)
+                    {
+                        dropdown.Show();
+                    }
+                }
+
+            }
+        }
+
+    }
+   /* void CheckUIInteraction()
+    {
+       
+    }
+   */
+    
+}
